72713a9854d26fc3a1e4a180a8d7384d96b16b07
rominarod <rominarod.ph@gmail.com>
2022-07-14 15:55:37 -0300

    clase4

---
+++ clase4/variables.js
@@ -0,0 +1,82 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// variables: let ---> "=" operador de asignacion //
+
+//var nombre = 'Romi';//
+
+//console.log(nombre);//
+
+//let numeroPreferido = 3;//
+
+//console.log(numeroPreferido);//
+
+//if(true) {
+    //let numeroPreferido = 5;//
+    //console.log(numeroPreferido);//
+//}//
+
+//const apellido = 'rodriguez'; const es una constante a traves de todo nuestro archivo, por lo que no podremos redefinirla, a diferencia de var o let//
+
+//const apellido = 'furgiuele'//
+
+//console.log(apellido);//
+
+
+// tipos de datos: number , strings (textos cortos o largos, utilizar comillas dobles o simples), boolean: true or False;//
+
+// tipos de datos especiales: not a number ( cuando algo no es un nro en si o una cadena de texto o un dato booleano), Null porq desconocemos el valor, undefined no existe valor, la var existe pero no tiene asignado valor//
+
+// mas tipos de datos: Objeto literal//
+
+// let miComputadora = {
+//    marca: 'mac'; (CLAVE)
+//    modelo: 'pro'
+//    encendida: true;
+//}//
+
+// array: lsta o arreglo, es un tipo de dato que nos permite hacer algo suiimilar al objeto literal, pero a diferencia de tener claves usa indices numericos
+
+// let serieFavorita = ["primer", "segundo", "tercero"]
+
+// operadores: de asignación (=) - aritmeticos ( + - * /) - modulo % 5%1 =1 me devuelve el resto de esa división - incremento y decremento ++ ó --
+// operadores de comparación: == si un valor es igual a otro, respuesta dato booleano, simple ----- estricta ===, pregunta por el valor y tipo de dato. valor es igual pero tipo de dato no 5!== '5'; TRUE
+//desigualdad simple y estricta  5 != '5'; FALSE solo pregunta por el valor y el valor no es distinto , estricta 5 !== '5' 
+
+// operadores lógicos, nos permiten unir sentencias de codigo que queremos consultar:
+// let dia = 'domingo';
+// let clima = 'soleado'
+// dia == 'domingo' && clima == 'soleado // true
+// or: || perguntar por varias sentencias a la vez, con que solo una se evalue como verdadera ya sera verdadera
+
+// concatenacion:
+// let nombre = 'juana'
+// let apellido = 'arco'
+
+// nombre + '' = apellido
+
+
+/* let primerNumero = 100
+let segundoNumero = 5
+
+let resultadoSuma = primerNumero + segundoNumero
+console.log(primerNumero + segundoNumero)
+
+let resultadoResta = primerNumero - segundoNumero
+console.log(primerNumero - segundoNumero)
+
+let resultadoMultiplicacion = primerNumero * segundoNumero
+console.log(primerNumero * segundoNumero)
+
+let resultadoDivision = primerNumero / segundoNumero
+console.log(primerNumero / segundoNumero) */
+
+
+/* let edad = 29
+    edad = 34
+
+const nombre = 'Romina'
+
+console.log( `hola ${nombre}`) //concatenar con backsticks
+ */
+
+
