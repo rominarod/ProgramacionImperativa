5b4d236acb0e1cfc765ab58c028e7ea347497b67
rominarod <rominarod.ph@gmail.com>
2022-07-14 15:54:44 -0300

    mas cositas

---
+++ clase5/funciones.js
@@ -0,0 +1,19 @@
+/* function anterior (a) {
+    return (a - 1)
+}
+
+let resultadoAnterior = anterior(10)
+console.log(resultadoAnterior);
+
+function triple (b) {
+    return (b * 3)
+}
+
+let resultadoTriple = triple(10)
+console.log(resultadoTriple);
+ */
+
+const toUpper = nombre => nombre.toUpperCase();
+
+console.log(toUpper("romina"));
+
